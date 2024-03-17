8b1f87720d990dad88d96aacdcf749cdb9bbf401
Suresh Melvin Sigera <sureshsigera@gmail.com>
2024-03-17 19:59:08 -0400

    added todo comments for App.test.js

--- react-to-do-frontend-starter/src/components/App.test.js
+++ react-to-do-frontend-starter/src/components/App.test.js
@@ -1,4 +1,20 @@
-const { Builder, By } = require("selenium-webdriver");
-import { render, fireEvent } from "@testing-library/react";
-import MyList from "../MyList";
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import MyList from '../../MyList';
 
+// Test to verify that the MyList component correctly renders its header
+test('Header contains the text "Things I should stop procrastinating:"', () => {
+    // The detailed implementation of this test will render the MyList component
+    // and then check if the header text content matches the expected string.
+});
+
+// Test to ensure that adding a new item through the form correctly updates the list
+test('Entering text into text input and clicking "Add it!" button adds the item to the list', () => {
+    // This test will simulate user actions: entering text into the input field and clicking the "Add it!" button.
+    // After the button click, it will verify that the new item has been added to the list of todos by checking the rendered output.
+});
+
+// Test to confirm that clicking the "Finished the list!" button clears the entire todo list
+test('Clicking on "Finished the list!" will delete all elements in the list', () => {
+    // This test will simulate the user action of clicking the "Finished the list!" button.
+    // It will then check to ensure that all items have been removed from the list, expecting the list's length to be 0.
+});
